2eb46fe8dc361167d80de24983b94e6e79f97b38
leovoon <voonlihhaur@gmail.com>
2020-05-11 19:24:54 +0800

    Update news.js

--- src/_data/news.js
+++ src/_data/news.js
@@ -6,7 +6,7 @@ require('dotenv').config()
 const getNews = async function () {
     try {
         const response = await axios.get(`
-        https://newsapi.org/v2/top-headlines?q=covid&apiKey=${process.env.NEWS_API_KEY}`);
+        https://newsapi.org/v2/top-headlines?country=my&q=covid&apiKey=${process.env.NEWS_API_KEY}`);
 
         return response.data
     } catch (error) {
