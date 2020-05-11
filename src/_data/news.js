aab3f4a85801652f2116b4e8366cd5a159a33cc0
leovoon <voonlihhaur@gmail.com>
2020-05-11 19:29:32 +0800

    Update news.js

--- src/_data/news.js
+++ src/_data/news.js
@@ -3,21 +3,18 @@ require('dotenv').config()
 
 
 
-const getNews = async function () {
+modules.export = 
+    if (sessionStorage.getItem("is_reloaded"))
+  async function () {
     try {
         const response = await axios.get(`
         https://newsapi.org/v2/top-headlines?country=my&q=covid&apiKey=${process.env.NEWS_API_KEY}`);
-
+        sessionStorage.setItem("is_reloaded", true);
         return response.data
     } catch (error) {
         console.error(error);
     }
 }
 
-const updateNews = () => {
-    document.location.reload();
-    getNews();
 
-}
 
-export default updateNews 
